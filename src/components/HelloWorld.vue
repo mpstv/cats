aaa41f4146c7ae0044e53b1e6563949882d12ed4
mpstv <54762420+mpstv@users.noreply.github.com>
2025-01-12 12:46:32 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref('Hello world')
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: coral;
+}
+</style>
