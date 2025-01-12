6f313564682848e4e26574ce3979dbac36f438c5
mpstv <54762420+mpstv@users.noreply.github.com>
2025-01-12 16:17:09 +0300

    Cats modals and favorites view

---
+++ src/components/Cats/CatModal.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import LikeButton from '../Buttons/LikeButton.vue'
+import CatImg from './CatImg.vue'
+
+const { catId } = defineProps<{ catId: string }>()
+</script>
+
+<template>
+  <div class="wrapper" @click="$emit('close')">
+    <div class="container" @click="($event) => $event.stopPropagation()">
+      <button class="close-button" @click="$emit('close')">✖</button>
+
+      <CatImg :cat-id="catId" />
+
+      <LikeButton :cat-id="catId" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  position: relative;
+  border: 1px dashed var(--text-color);
+  height: 85vh;
+  width: 85vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+  background-color: var(--background-color);
+}
+
+.close-button {
+  all: unset;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
+  color: var(--text-color);
+}
+</style>
--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,7 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import CatImg from '../components/Cats/CatImg.vue'
+import CatModal from '../components/Cats/CatModal.vue'
+import { useCatsStore } from '@/stores/cats'
+
+const activeCat = ref('')
+const store = useCatsStore()
+</script>
 
 <template>
-  <main>
-    <h1>This is favorites page</h1>
-  </main>
+  <div class="container">
+    <div v-for="cat in store.cats" :key="cat" class="cat">
+      <CatImg :key="cat" :cat-id="cat" @click="() => (activeCat = cat)" />
+    </div>
+  </div>
+
+  <CatModal v-if="activeCat" :cat-id="activeCat" @close="() => (activeCat = '')" />
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.cat {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  cursor: pointer;
+  --cat-img-height: 200px;
+  --cat-img-width: 200px;
+}
+</style>
